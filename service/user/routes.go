e68474d1ba88a333e629e6cb5536e2ace21bf33e
BerkWiro <berkcicekler7@gmail.com>
2024-11-10 00:22:01 +0300

    google login added

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -11,6 +13,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/oauth2"
 )
 
 type UserServiceHandler struct {
@@ -24,6 +27,8 @@ func UserServiceNewHandler(repository repository.UsersRepo) *UserServiceHandler 
 func (h *UserServiceHandler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/user/login", h.handleLogin).Methods("POST")
 	router.HandleFunc("/user/register", h.handleRegister).Methods("POST")
+	router.HandleFunc("/user/google/login", h.handleGoogleLogin).Methods("GET")
+	router.HandleFunc("/user/google/callback", h.handleGoogleCallback).Methods("GET")
 }
 
 func (h *UserServiceHandler) handleLogin(w http.ResponseWriter, r *http.Request) {
@@ -115,4 +120,63 @@ func (h *UserServiceHandler) handleRegister(w http.ResponseWriter, r *http.Reque
 	userResponse.Token = token
 
 	utils.WriteJSON(w, http.StatusCreated, userResponse)
+}
+
+// GOOGLE 
+
+func (h *UserServiceHandler) handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	url := auth.GoogleOauthConfig.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func (h *UserServiceHandler) handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "Code not found", http.StatusBadRequest)
+		return
+	}
+
+	token, err := auth.GoogleOauthConfig.Exchange(context.Background(), code)
+
+	if err != nil {
+		http.Error(w, "Failed to exchange token", http.StatusInternalServerError)
+		return
+	}
+
+	client := auth.GoogleOauthConfig.Client(context.Background(), token)
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var gmailData model.GmailData
+	if err := json.NewDecoder(resp.Body).Decode(&gmailData); err != nil {
+		http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
+		return
+	}
+
+	user, err := h.repository.FindUserByEmail(gmailData.Email)
+
+	if user == nil {
+		user = &model.User{ID: primitive.NewObjectID(), Email: gmailData.Email, UserName: gmailData.GivenName}
+		_, err = h.repository.InsertUser(user)
+		if err != nil{
+			http.Error(w, "Database error", http.StatusInternalServerError)
+			return
+		}
+	}
+	
+	JWTtoken, err := auth.CreateJWT(user.ID.Hex())
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	userResponse := model.UserLoginResponseFromUser(user)
+	userResponse.Token = JWTtoken
+
+	utils.WriteJSON(w, http.StatusCreated, userResponse)
+
 }
